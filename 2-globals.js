d49609d53b139f9960223c3e992849945d07cc62
jackyyim881 <jackyyim881@hotmail.com>
2022-01-15 15:59:30 +0800

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,19 @@
+// Modules
+const secret = 'super secret'
+const john ='john'
+const peter = 'peter'
+
+const sayHi = (name) =>{
+    console.log(`Hello there ${name}`);
+}
+
+sayHi('susan')
+sayHi(john)
+sayHi(peter)
+
+const yo = (names)=>{
+
+    console.log(`hello${names}`);
+}
+
+yo('names')
